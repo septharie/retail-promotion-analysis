764f308984546f8cd8b305eb71a3e7560351eab7
septharie <165117494+septharie@users.noreply.github.com>
2026-07-27 15:16:29 +0700

    Create category_performance.sql

---
+++ sql/03_analysis/category_performance.sql
@@ -0,0 +1,21 @@
+SELECT 
+    category,
+    
+    -- Order & Volume Metrics
+    COUNT(DISTINCT sales_code) AS total_orders,
+    SUM(sales_quantity) AS total_units_sold,
+    
+    -- Financial Metrics
+    CAST(ROUND(SUM(revenue), 2) AS NUMERIC) AS total_revenue,
+
+    -- % Revenue Contribution
+    ROUND(
+        (SUM(revenue) * 100.0) / SUM(SUM(revenue)) OVER(), 2
+    ) AS revenue_share_pct,
+
+    -- Basket Metric
+    ROUND(SUM(revenue) / COUNT(DISTINCT sales_code), 2) AS avg_order_value
+
+FROM `sales_summary`
+GROUP BY category
+ORDER BY total_revenue DESC;
